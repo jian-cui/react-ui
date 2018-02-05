a52dad3f67894ec8109d63829725472152ef91a0
acui145@gmail.com <acui145@gmail.com>
2018-02-05 18:41:31 +0800

    修改eslint错误

--- components/button/button-group.jsx
+++ components/button/button-group.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import classNames from 'classnames';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export default class ButtonGroup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
   render() {
-    const { prefixCls='react-btn-group', className, children } = this.props;
-    
+    const { prefixCls, className, children } = this.props;
     const classes = classNames(prefixCls, {
 
     }, className);
-    return (
-      <div className={classes}>
-        { children }
-      </div>)
+    return <div className={classes}>{ children }</div>;
   }
 }
+
+ButtonGroup.defaultProps = {
+  prefixCls: 'react-btn-group',
+  className: undefined,
+  children: undefined
+};
+ButtonGroup.propTypes = {
+  prefixCls: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node
+};
--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import Icon from '../icon/index.jsx';
+import Icon from '../icon';
 import './style/style.less';
 
 export default class Button extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       clicked: false,
-    }
+    };
+    this.timeoutID = null;
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  timeoutID;
-
-  componentWillReceiveProps() {
-
-  }
-  handleClick (e) {
+  handleClick(e) {
     // Add click effect
     this.setState({ clicked: true });
     clearTimeout(this.timeoutID);
-    this.timeoutID = setTimeout(() => this.setState({ clicked: false }), 500);
+    this.timeoutID = setTimeout(() => {
+      this.setState({ clicked: false });
+    }, 500);
 
     const onClick = this.props.onClick;
     if (onClick) {
       onClick(e);
     }
   }
-  render () {
+  render() {
     const {
-      type, shape, size, className, children, icon, prefixCls, loading, ghost, disabled, href, style
-     } = this.props;
+      type, buttonType, shape, size, className, children,
+      icon, prefixCls, loading, ghost, disabled, href, style,
+    } = this.props;
 
     let sizeCls = '';
     switch (size) {
@@ -45,7 +46,6 @@ export default class Button extends React.Component {
         break;
     }
 
-
     const { clicked } = this.state;
 
     const classes = classNames(prefixCls, className, {
@@ -54,42 +54,51 @@ export default class Button extends React.Component {
       [`${prefixCls}-${sizeCls}`]: sizeCls,
       [`${prefixCls}-loading`]: loading,
       [`${prefixCls}-clicked`]: clicked,
-      [`${prefixCls}-background-ghost`]: ghost
-    })
+      [`${prefixCls}-background-ghost`]: ghost,
+    });
     const ComponentName = href ? 'a' : 'button';
     const iconType = loading ? 'loading' : icon;
     const iconNode = iconType ? <Icon type={iconType} /> : null;
-    const kids = React.Children.map(children, child => <span>{child}</span>);
+    const kids = React.Children.map(children, (child) => {
+      return <span>{child}</span>;
+    });
     const dis = disabled ? 'disabled' : '';
     return (
       <ComponentName
-        type='button'
-        className={ classes }
-        onClick={ this.handleClick.bind(this) }
-        disabled= { disabled }
-        href= { href }
-        style= { style }
+        type={href ? undefined : buttonType}
+        className={classes}
+        onClick={this.handleClick}
+        disabled={dis}
+        href={href}
+        style={style}
       >
         { iconNode }
         { kids }
       </ComponentName>
-    )
+    );
   }
 }
 
 Button.defaultProps = {
   type: 'default',
+  shape: undefined,
+  icon: undefined,
+  buttonType: 'button',
   prefixCls: 'react-btn',
   loading: false,
   ghost: false,
   size: 'medium',
-  disabled: false
-}
+  disabled: false,
+  className: '',
+  href: undefined,
+  children: undefined,
+};
 
 Button.propTypes = {
   type: PropTypes.string,
   shape: PropTypes.string,
   icon: PropTypes.string,
+  buttonType: PropTypes.oneOf(['button']),
   prefixCls: PropTypes.string,
   loading: PropTypes.bool,
   ghost: PropTypes.bool,
@@ -98,4 +107,5 @@ Button.propTypes = {
   className: PropTypes.string,
   onClick: PropTypes.func,
   href: PropTypes.string,
-}
+  children: PropTypes.node,
+};
