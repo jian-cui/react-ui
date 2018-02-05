fc9bc3bc860fe6c307f4a59164f99ebd0661de20
acui145@gmail.com <acui145@gmail.com>
2018-02-05 16:40:11 +0800

    add styles to Icon

--- components/icon/index.jsx
+++ components/icon/index.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import './style/style.less';
 
 export default class Icon extends React.Component {
-  constructor (props) {
-    super(props);
-  }
-  render () {
-    const { type, className = '', spin } = this.props;
+  render() {
+    const { type, className } = this.props;
     let typeClass = type;
-    if (type == 'loading') typeClass = 'loading2';
+    if (type === 'loading') typeClass = 'loading2';
     const classes = classNames({
       reacticon: true,
       [`icon-${typeClass}`]: true,
     }, className);
-    return <i className={ classes } />
+    return <i className={classes} />;
   }
 }
+
+Icon.defaultProps = {
+  type: undefined,
+  className: '',
+};
+
+Icon.propTypes = {
+  type: PropTypes.string,
+  className: PropTypes.string,
+};
